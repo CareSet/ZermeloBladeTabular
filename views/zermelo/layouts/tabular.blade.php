cb0ca0c0194d12806176842ed5274e67cd8eb45f
Fred Trotter <fred.trotter@gmail.com>
2018-08-04 01:59:59 -0500

    update fontawesome

--- views/zermelo/layouts/tabular.blade.php
+++ views/zermelo/layouts/tabular.blade.php
@@ -5,7 +5,7 @@
 
 <link rel="stylesheet" type="text/css" href="/vendor/CareSet/css/bootstrap.min.css"/>
 <link rel="stylesheet" type="text/css" href="/vendor/CareSet/css/datatables.min.css"/>
-<link href="//use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+<link href="//use.fontawesome.com/releases/v5.2.0/css/all.css" rel="stylesheet">
 <link rel="stylesheet" type="text/css" href="/vendor/CareSet/css/caresetreportengine.report.css"/>
 </head>
 <body>
