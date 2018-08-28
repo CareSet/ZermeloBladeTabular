dce307c5a8230a9f85dce3d5188298bee5e1d594
Ken Chapple <ken.chapple@gmail.com>
2018-08-28 10:42:40 -0400

    Update to Bootstrap 4.1 (#51)

--- views/zermelo/layouts/tabular.blade.php
+++ views/zermelo/layouts/tabular.blade.php
@@ -3,8 +3,9 @@
 
 <title>{{ $presenter->getReport()->getReportName()  }}</title>
 
-<link rel="stylesheet" type="text/css" href="/vendor/CareSet/css/bootstrap.min.css"/>
-<link rel="stylesheet" type="text/css" href="/vendor/CareSet/css/datatables.min.css"/>
+
+<link rel="stylesheet" type="text/css" href="/vendor/CareSet/bootstrap/css/bootstrap.min.css"/>
+<link rel="stylesheet" type="text/css" href="/vendor/CareSet/css/dataTables.bootstrap4.min.css"/>
 <link href="//use.fontawesome.com/releases/v5.2.0/css/all.css" rel="stylesheet">
 <link rel="stylesheet" type="text/css" href="/vendor/CareSet/css/caresetreportengine.report.css"/>
 </head>
