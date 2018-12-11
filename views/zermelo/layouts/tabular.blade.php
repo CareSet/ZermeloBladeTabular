75192ad2846eb4ee5de8a24e61b05b375f582d18
Ken Chapple <ken.chapple@gmail.com>
2018-12-11 13:50:26 -0500

    configurable bootstrap css

--- views/zermelo/layouts/tabular.blade.php
+++ views/zermelo/layouts/tabular.blade.php
@@ -4,7 +4,7 @@
     <title>{{ $presenter->getReport()->getReportName()  }}</title>
 
     <link href="//use.fontawesome.com/releases/v5.2.0/css/all.css" rel="stylesheet" />
-    <link rel="stylesheet" type="text/css" href='{{ asset("vendor/CareSet/bootstrap/css/bootstrap.min.css") }}' />
+    <link rel="stylesheet" type="text/css" href='{{ bootstap_css() }}' />
     <link rel="stylesheet" type="text/css" href='{{ asset("vendor/CareSet/css/caresetreportengine.report.css") }}' />
     <link rel="stylesheet" type="text/css" href='{{ asset("vendor/CareSet/datatables/datatables.min.css") }}' />
 
