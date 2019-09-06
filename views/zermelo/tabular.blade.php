ea27e937f443ea7b5f4d213d7bbd0410b19b4c08
Ken Chapple <ken.chapple@gmail.com>
2019-09-06 09:49:36 -0400

    Upgrade bootstrap to 4.3.1 and JQuery to 3.4.1

--- views/zermelo/tabular.blade.php
+++ views/zermelo/tabular.blade.php
@@ -132,7 +132,7 @@
 		</div>
 	</div>
 </div>
-<script type="text/javascript" src="/vendor/CareSet/js/jquery-3.3.1.min.js"></script>
+<script type="text/javascript" src="/vendor/CareSet/js/jquery-3.4.1.min.js"></script>
 <script type="text/javascript" src="/vendor/CareSet/datatables/datatables.js"></script>
 <script type="text/javascript" src="/vendor/CareSet/js/popper.min.js"></script>
 <script type="text/javascript" src="/vendor/CareSet/bootstrap/js/bootstrap.js"></script>
